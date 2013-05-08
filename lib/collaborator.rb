a6f14f33721cc75b7ee9b36f1717c85f886a081d
Matt Evans <matt@thebetacollective.co.uk>
2013-05-08 14:25:49 +0100

    lunch time updates

--- lib/collaborator.rb
+++ lib/collaborator.rb
@@ -3,13 +3,12 @@ require 'sinatra/base'
 require 'mongoid'
 #this refers to the mongoid gem
 
-#we can't use git to save our lives!!!
-
-  #ENV["RACK_ENV"] = "production"
+require 'post_message'
+#trying to link to another file with a class within it
 
 class Collaborator < Sinatra::Base
+  #this class is a controller
   set :views, File.join(File.dirname(__FILE__), '../views')
-
   Mongoid.load!(File.join(File.dirname(__FILE__),"mongoid.yml"))
 
   get '/' do
