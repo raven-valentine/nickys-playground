f49fbb5de501a565cc8020f349cc8d1dad4ff2fe
Enrique Comba Riepenhausen <ecomba@ecomba.org>
2013-05-09 14:50:27 +0100

    Fixed mangled specs

--- spec/collaborator_spec.rb
+++ spec/collaborator_spec.rb
@@ -12,12 +12,12 @@ describe Collaborator do
 		it 'greets us' do 
 			get '/' #sends a get request to Sinatra
 
-			last_response.body.should eq 'Hello Collaborator!' #expects a response from Sinatra
+			last_response.body.should eq 'Hey there' #expects a response from Sinatra
 		end
 
 		context 'creating a new post' do #groups tests that fit together
 			it 'saves a new post' do 
-				Post.should_receive(:create).with(message: 'Hey test')
+				Post.should_receive(:create).with(content: 'Hey test')
 			# a class post will receive a method create with message 'Hey test'
 				post '/mock-groupname', { :message => 'Hey test'}
 			end
