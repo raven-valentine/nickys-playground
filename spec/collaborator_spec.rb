262dc39db7a63ca0f172c0d79e985c2b6da25157
Steve Goulden <stevenjgoulden@hotmail.co.uk>
2013-05-09 14:42:41 +0100

    my stuff after Enrique added the html stuff to Nicky and I had to save my files before merging hers

--- spec/collaborator_spec.rb
+++ spec/collaborator_spec.rb
@@ -3,16 +3,19 @@ require 'group'
 require 'rspec'
 require 'rack/test' # rack is a specification close to the TCP/IP protocol. rack test is a set of testing helpers allowing test of web applications
 
-describe Group do #describing the Collaborator class
+describe Collaborator do #describing the Collaborator class
+  include Mongoid::Document
   include Rack::Test::Methods  # importing methods to test as if was a web applciation. see the sinatra documentation for info on rack tests.
 
   def app
   	Collaborator #this is Sinatra::Application. this is taking the Collaborator class, and making an instance available in the whole rspec env.
   end
+#this makes the app Collaborator available to the test so can send things to it
+  
+  it 'returns all the groups in the database' do
+  	Group.should receive (:all)
+		get '/list-of-groups'
 
-  it 'has a group called hiking'
-      get '/list-of-groups'
-      hiking = Group.where(name: "hiking").first
   end
 
 end
